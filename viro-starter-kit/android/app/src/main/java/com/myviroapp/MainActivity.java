0a895fa46f13cb096759fbfeec0e0e63b4e7dd17
hanbeulYou <hanbeul.you@gmail.com>
2023-05-02 09:28:51 +0900

    chore: 파일 복구

---
+++ viro-starter-kit/android/app/src/main/java/com/myviroapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.myviroapp;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "myviroapp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
